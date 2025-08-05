d26892407b43e5e8ca94ed49ef187597246dec68
Azim <annayev.azim@gmail.com>
2025-08-04 21:12:37 -0400

    Fix all UI component imports to use explicit .js extension for Vite compatibility

--- habitos/frontend/src/components/ui/toast.jsx
+++ habitos/frontend/src/components/ui/toast.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ToastPrimitives from "@radix-ui/react-toast";
 import { cva } from "class-variance-authority";
 import { X } from "lucide-react";
-import { cn } from "../../lib/utils";
+import { cn } from "../../lib/utils.js";
 
 const ToastProvider = ToastPrimitives.Provider;
 
